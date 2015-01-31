610ceedbc90d0b397f5430a6e92fa4cdeb1f6e18
Marylia Gutierrez <marylia@user138-39.wireless.utoronto.ca>
2015-01-31 05:01:08 -0500

    Play

--- scripts/jukebox.js
+++ scripts/jukebox.js
@@ -1,25 +1,50 @@
+var endpoint = "http://slack-jukebox.mybluemix.net";
+
 module.exports = function (robot) {
+  var get = function (path, prefix) {
+    prefix = prefix || "";
+    robot.http(endpoint + "/" + path).get()(function (err, res, body) {
+      if (res.statusCode == 200) {
+        msg.send(prefix + body);
+      }
+    });
+  };
+
   robot.respond(/h[ae]lp/i, function (msg) {
     msg.send("play     - Add a song to the queue");
-    msg.send("skip     - Skip to the next song in the queue");
+    // msg.send("skip     - Skip to the next song in the queue");
     msg.send("playlist - Show next 10 songs in the queue");
     msg.send("current  - Show song currently playing");
   });
 
   robot.respond(/play (.*)/i, function (msg) {
-    msg.send("Adding '" + msg.match[1] + "' to the queue!");
+    data = JSON.stringify({
+      song: msg.match[1]
+    });
+
+    msg.send(data);
+
+    robot.http(endpoint + "/add")
+      .header("Content-Type", "application/json")
+      .post(data)(function (err, res, body) {
+        if (res.statusCode == 200) {
+          msg.send("Adding '" + body + "' to the queue!");
+        } else {
+          msg.send("Nope.");
+        }
+      });
   });
 
-  robot.respond(/skip/i, function (msg) {
-    msg.send("Boo!");
-  });
+  // robot.respond(/skip/i, function (msg) {
+  //   msg.send("Boo!");
+  // });
 
   robot.respond(/playlist/i, function (msg) {
-    msg.send("Up next:");
+    get("playlist");
   });
 
   robot.respond(/current\ ?(song)?/i, function (msg) {
-    msg.send("Now playing: '" + "song" + "'");
+    get("current", "Now playing: ");
   });
 
   robot.hear(/google/, function (msg) {
